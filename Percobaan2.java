e6e2c12641c7c0aef0c05fdba879808f6fb934cb
Nizam El <nizamel1402@gmail.com>
2024-12-01 21:44:52 +0700

    percobaan 2 jawaban pertanyaan no 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 public class Percobaan2 {
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
             return (x * hitungPangkat(x, y - 1));
         }
     }
-    
+
+    static void DeretPangkat(int x, int y) {
+        for (int i = 0; i < y; i++) {
+            System.out.print(x);  
+            if (i < y - 1) {
+                System.out.print(" x ");  
+            }
+        }
+        System.out.print(" x 1 = ");  
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int bilangan, pangkat;
@@ -16,7 +26,8 @@ public class Percobaan2 {
         bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
+        DeretPangkat(bilangan, pangkat);
 
-        System.out.println(hitungPangkat(bilangan, pangkat));   
+        System.out.println(hitungPangkat(bilangan, pangkat));
     }
 }
